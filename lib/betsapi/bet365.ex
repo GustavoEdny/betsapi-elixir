2e7e7aea91c25fe4e720afbf56688bbc406905b4
Gustavo Edny <gustavoedny@gmail.com>
2020-04-14 20:55:10 -0300

    inplay method added

--- lib/betsapi/bet365.ex
+++ lib/betsapi/bet365.ex
@@ -1,3 +1,9 @@
 defmodule Betsapi.Bet365 do
+  @inplay "v1/bet365/inplay"
 
+  @spec inplay(binary, list | nil) :: {:error, any} | {:ok, Tesla.Env.t()}
+  def inplay(token, query \\ nil) do
+    token
+      |> Betsapi.call(@inplay, query)
+  end
 end
