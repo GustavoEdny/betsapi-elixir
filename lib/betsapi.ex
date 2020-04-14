72f87fa90b964f486e6b37dbf96702c99e067eb0
Gustavo Edny <gustavoedny@gmail.com>
2020-04-14 20:52:08 -0300

    adding call method to handle requests

--- lib/betsapi.ex
+++ lib/betsapi.ex
@@ -1,18 +1,12 @@
 defmodule Betsapi do
-  @moduledoc """
-  Documentation for `Betsapi`.
-  """
+  use Tesla, only: [:get], docs: false
 
-  @doc """
-  Hello world.
+  plug Tesla.Middleware.BaseUrl, "https://api.betsapi.com"
 
-  ## Examples
-
-      iex> Betsapi.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def call(token, endpoint, query \\ nil) do
+    case query do
+      query when is_list(query) -> get endpoint, query: [token: token] ++ query
+      _____ when true -> get endpoint, query: [token: token]
+    end
   end
 end
