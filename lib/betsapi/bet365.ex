29bbdb680ff81c1b72f23465c5c74b88a451d508
Gustavo Edny <gustavoedny@gmail.com>
2020-04-14 21:20:50 -0300

    refactoring query default value to be a empty list

--- lib/betsapi/bet365.ex
+++ lib/betsapi/bet365.ex
@@ -1,27 +1,25 @@
 defmodule Betsapi.Bet365 do
   @inplay "v1/bet365/inplay"
 
-  @spec inplay(binary, list | nil) :: {:error, any} | {:ok, Tesla.Env.t()}
-  def inplay(token, query \\ nil) do
+  @spec inplay(binary, [any]) :: {:error, any} | {:ok, Tesla.Env.t()}
+  def inplay(token, query \\ []) do
     token
       |> Betsapi.call(@inplay, query)
   end
 
   @inplay_filter "v1/bet365/inplay_filter"
 
-  @spec inplay_filter(binary, list | nil) :: {:error, any} | {:ok, Tesla.Env.t()}
-  def inplay_filter(token, query \\ nil) do
+  @spec inplay_filter(binary, [any]) :: {:error, any} | {:ok, Tesla.Env.t()}
+  def inplay_filter(token, query \\ []) do
     token
       |> Betsapi.call(@inplay_filter, query)
   end
 
   @inplay_event "v1/bet365/event"
 
-  def inplay_event(token, fixture_id, query \\ nil) do
-    case query do
-      query when is_list(query) -> Betsapi.call(token, @inplay_event, query ++ [FI: fixture_id])
-      _____ when true -> Betsapi.call(token, @inplay_event, [FI: fixture_id])
-    end
+  @spec inplay_event(binary, integer, [any]) :: {:error, any} | {:ok, Tesla.Env.t()}
+  def inplay_event(token, fixture_id, query \\ []) do
+    Betsapi.call(token, @inplay_event, query ++ [FI: fixture_id])
   end
 
   @upcoming "v1/bet365/upcoming"
