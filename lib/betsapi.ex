eb6ee56bb722390304e33d75638b9aa2e56db08c
Gustavo Edny <gustavoedny@gmail.com>
2020-04-14 21:50:40 -0300

    fixing query default value to be a empty list

--- lib/betsapi.ex
+++ lib/betsapi.ex
@@ -3,10 +3,7 @@ defmodule Betsapi do
 
   plug Tesla.Middleware.BaseUrl, "https://api.betsapi.com"
 
-  def call(token, endpoint, query \\ nil) do
-    case query do
-      query when is_list(query) -> get endpoint, query: [token: token] ++ query
-      _____ when true -> get endpoint, query: [token: token]
-    end
+  def call(token, endpoint, query \\ []) do
+    get endpoint, query: [token: token] ++ query
   end
 end
