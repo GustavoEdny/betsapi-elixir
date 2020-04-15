e6c46e4cceef0ebc1d9e87ff952851d1d84b381a
Gustavo Edny <gustavoedny@gmail.com>
2020-04-14 21:00:44 -0300

    inplay_filter method added

--- lib/betsapi/bet365.ex
+++ lib/betsapi/bet365.ex
@@ -6,4 +6,12 @@ defmodule Betsapi.Bet365 do
     token
       |> Betsapi.call(@inplay, query)
   end
+
+  @inplay_filter "v1/bet365/inplay_filter"
+
+  @spec inplay_filter(binary, list | nil) :: {:error, any} | {:ok, Tesla.Env.t()}
+  def inplay_filter(token, query \\ nil) do
+    token
+      |> Betsapi.call(@inplay_filter, query)
+  end
 end
